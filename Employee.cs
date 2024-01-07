4a18dd439d62e6ff30aa037aae44a4ff22317516
meilizheng <116130097+meilizheng@users.noreply.github.com>
2024-01-07 10:24:53 -0800

    Add Commend

--- Employee.cs
+++ Employee.cs
@@ -8,10 +8,13 @@ namespace LinearSearch
 {
     public class Employee
     {
+        //filed
         string _name;
         string _department;
         string _position;
 
+
+        //Construction
         public Employee(string name, string department, string position)
         {
             _name = name;
@@ -19,6 +22,7 @@ namespace LinearSearch
             _position = position;
         }
 
+        //Property
         public string Name { get => _name; set => _name = value; }
         public string Department { get => _department; set => _department = value; }
         public string Position { get => _position; set => _position = value; }
--- Program.cs
+++ Program.cs
@@ -1,42 +1,43 @@
-﻿namespace LinearSearch
+﻿namespace LinearSearch   //nameSpace
 {
-    internal class Program
+    internal class Program  //class
     {
+        //Declare the global arrays
         static Employee[] employees;
         static string[] employeeDepartment;
         static string[] employeePosition;
-        static void Main(string[] args)
+        static void Main(string[] args)  //Main
         {
 
             Preload();
 
-            Contains();
+            Contains();//Call Contains Method
 
             Console.WriteLine("Employee By Position");
-            Console.WriteLine((employeeByPosition(employeePosition, "Accountant")));
-            Console.WriteLine((employeeByPosition(employeePosition, "CEO")));
+            Console.WriteLine((employeeByPosition(employeePosition, "Accountant")));// 1
+            Console.WriteLine((employeeByPosition(employeePosition, "CEO")));// -1
 
             Console.WriteLine("All Employees of a Department");
-            Console.WriteLine(AllEmployeeOfDepartment(employeeDepartment, "IT").Count);
-            Console.WriteLine(AllEmployeeOfDepartment(employeeDepartment, "Payroll").Count);
+            Console.WriteLine(AllEmployeeOfDepartment(employeeDepartment, "IT").Count); //2
+            Console.WriteLine(AllEmployeeOfDepartment(employeeDepartment, "Payroll").Count); //0
 
             Console.WriteLine("All Employees of Positon");
             List<Employee> result = AllEmployeeOfPosition(employees, "Software Engineer");
-            Console.WriteLine(result.Count);
+            Console.WriteLine(result.Count);  // 2
             foreach (Employee employee in result) { Console.WriteLine($"{employee.Name} - {employee.Position}"); }
-            Console.WriteLine(AllEmployeeOfPosition(employees, "Payroll").Count);
-        }
+            Console.WriteLine(AllEmployeeOfPosition(employees, "Payroll").Count);  //0
+        } //End Main
 
-        public static bool ContainsDepartment(string[] departmentList, string searchKey)
+        public static bool ContainsDepartment(string[] departmentList, string searchKey) //ContainsDepartment method check if the departmentName in department list
         {
-            foreach (string employee in departmentList)
+            foreach (string department in departmentList) //use foreach check every department name in the list. Linear Search
             {
-                if (employee == searchKey)
+                if (department == searchKey)  //if found
                 {
-                    return true;
+                    return true; //return true
                 }
             }
-            return false;
+            return false;  //else return false
         }
 
         public static void Contains()
@@ -45,15 +46,15 @@
             string searchDepartment = "Marketing";
             if (ContainsDepartment(employeeDepartment, searchDepartment))
             {
-                Console.WriteLine($"true. {searchDepartment} includ in employeeDepartment List.");
+                Console.WriteLine($"true. {searchDepartment} includ in employeeDepartment List.");//if Marketing in department list return true
             }
             else
             {
-                Console.WriteLine($"false. {searchDepartment} doesn't includ in employeeDepartment List.");
+                Console.WriteLine($"false. {searchDepartment} doesn't includ in employeeDepartment List.");//else return false
             }
         }
 
-        public static int employeeByPosition(string[] positionList, string searchKey)
+        public static int employeeByPosition(string[] positionList, string searchKey)  //employeeByPosition method check position's index
         {
             for (int i = 0; i < positionList.Length; i++)
             {
@@ -62,15 +63,15 @@
                     return i;
                 }    
             }
-            return -1;
+            return -1;  //return -1 if not found
         }
 
-        public static List<int> AllEmployeeOfDepartment(string[] employeeList, string searchKey)
+        public static List<int> AllEmployeeOfDepartment(string[] departmentList, string searchKey)  //AllEmployeeOfDepartment method check department total number in employeeDepartment
         {
             List<int> result = new List<int>();
-            for(int i = 0; i < employeeList.Length; i++)
+            for(int i = 0; i < departmentList.Length; i++)
             {
-                if (employeeList[i] == searchKey)
+                if (departmentList[i] == searchKey)
                 {
                     result.Add(i);
                 }
@@ -78,7 +79,7 @@
             return result;
         }
 
-        public static List<Employee> AllEmployeeOfPosition(Employee[] employees, string searchKey)
+        public static List<Employee> AllEmployeeOfPosition(Employee[] employees, string searchKey) //AllEmployeeOfPosition method check total position in employee list
         {
             List<Employee> employeeOfPosition = new List<Employee>();
             foreach(Employee employee in employees)
@@ -134,6 +135,6 @@
                 new Employee("Daniel Taylor", "Legal", "Legal Counsel"),
                 new Employee("Grace Turner", "Administration", "Administrative Assistant")
             };
-        }
-    }
-}
+        } //End Preload
+    } //End Class
+} //End nameSpace
