8b5b2e189cb54a8aa4c68ff861b6c09b5d8839dc
meilizheng <116130097+meilizheng@users.noreply.github.com>
2024-01-06 23:04:18 -0800

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    public class Employee
+    {
+        string _name;
+        string _department;
+        string _position;
+
+        public Employee(string name, string department, string position)
+        {
+            _name = name;
+            _department = department;
+            _position = position;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string Department { get => _department; set => _department = value; }
+        public string Position { get => _position; set => _position = value; }
+
+        public override string ToString()
+        {
+            return $"{_name} {_department} {_position}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LinearSearch
+{
+    internal class Program
+    {
+        static Employee[] employees;
+        static void Main(string[] args)
+        {
+
+            Preload();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+
+        static void Preload()
+        {
+            employees = new Employee[]
+            {
+                new Employee("John Doe", "Marketing", "Manager"),
+                new Employee("Alice Smith", "Human Resources", "HR Specialist"),
+                new Employee("Bob Johnson", "Finance", "Accountant"),
+                new Employee("Sara Miller", "IT", "Software Engineer"),
+                new Employee("David Williams", "Operations", "Operations Manager"),
+                new Employee("Emily Davis", "Sales", "Sales Representative"),
+                new Employee("Michael Brown", "Customer Service", "Customer Support Agent"),
+                new Employee("Olivia Moore", "Research and Development", "Research Scientist"),
+                new Employee("Daniel Taylor", "Legal", "Legal Counsel"),
+                new Employee("Grace Turner", "Administration", "Administrative Assistant")
+            };
+        }
+    }
+}
